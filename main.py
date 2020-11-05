7c81e0b4f8216fb993cc6b1bff81e474d31d1f67
Flonky <64008721+Flonky@users.noreply.github.com>
2020-11-05 18:14:45 +0100

    Initial Release

---
+++ main.py
@@ -0,0 +1,14 @@
+import string
+import random
+
+def get_length():
+	length = input("\n	Password length: 	")
+	return int(length)
+
+ASCII = string.ascii_uppercase + string.ascii_lowercase
+DIGITS = string.digits
+SPECIAL_CHARACTERS = string.punctuation
+PASSWORD = ''.join(random.choice(ASCII + DIGITS + SPECIAL_CHARACTERS) for _ in range(get_length()))
+
+if __name__ == '__main__':
+	print("\n	Passord: 		" + PASSWORD)
